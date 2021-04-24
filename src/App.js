7bea64d5c49e08075d43c4d3271f7595baf71a97
Dolores <lolygalan19@gmail.com>
2021-04-24 17:51:28 +0200

    titulares y suplentes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Jugadores from './components/Jugadores';
 import { Provider } from 'react-redux';
 import store from './store'
+import EquipoSeleccionado from './components/equiposeleccionado';
 
 const App = () => (
   <Provider store={store}>
     <main>
       <h1>Plantilla de futbolistas</h1>
       <Jugadores/>
+      <EquipoSeleccionado />
     </main>
   </Provider>
 )
